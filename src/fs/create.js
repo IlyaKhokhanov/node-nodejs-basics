3912ed5ca3bca9ded4d613321d5fa0f205031138
IlyaKhokhanov <hohanov.ilya@gmail.com>
2024-10-06 22:39:50 +0300

    feat: add create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+import { stat, writeFile } from 'fs/promises';
+
+const path = fileURLToPath(import.meta.url);
+const pathFile = dirname(path) + '/files/fresh.txt';
+
 const create = async () => {
-    // Write your code here 
-};
+    const checkFile = await stat(pathFile)
+        .then((res) => res.isFile())
+        .catch((err) => err.code === 'ENOENT' ? false : err);
+    if (!checkFile) {
+        return await writeFile(pathFile, 'I am fresh and young');
+    } else {
+        throw new Error('FS operation failed');
+    }
+}
+
 
 await create();
