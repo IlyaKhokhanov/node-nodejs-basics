096d5d9fed71fecbf0c64552233461ef1be1041e
IlyaKhokhanov <hohanov.ilya@gmail.com>
2024-10-07 18:47:08 +0300

    feat: add read.js and write.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,23 @@
+import {join, dirname} from 'path';
+import {fileURLToPath} from 'url';
+import {createReadStream} from 'fs';
+import {stdout} from 'process';
+
+const __fileName = fileURLToPath(import.meta.url);
+const __dirname = dirname(__fileName);
+const path = join(__dirname, 'files');
+const sourcePath = join(path, 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+    const readable = createReadStream(sourcePath);
+
+    readable.pipe(stdout);
+
+    readable.on('end', () => readable.unpipe(stdout));
+
+    readable.on('error', (err) => {
+        throw new Error(err);
+    })
 };
 
 await read();
